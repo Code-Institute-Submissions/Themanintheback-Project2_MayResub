ba1053f33debc66ef336e3ee5bb02cd60828abc5
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-03-28 03:55:24 +0000

    Add computer random number function to js page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,32 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
    userChoice = e.target.id
    userChoiceDisplay.innerHTML = userChoice
+   generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+   const randomNumber = Math.floor(Math.random() * 5) + 1
+   
+   if (randomNumber === 1) {
+      computerChoice = 'rock'
+   }
+   if (randomNumber === 2) {
+      computerChoice = 'paper'
+   }
+   if (randomNumber === 3) {
+      computerChoice = 'scissors'
+   }
+   if (randomNumber === 4) {
+      computerChoice = 'lizard'
+   }
+   if (randomNumber === 5) {
+      computerChoice = 'spock'
+   }
+   computerChoiceDisplay.innerHTML = computerChoice
+}
+
