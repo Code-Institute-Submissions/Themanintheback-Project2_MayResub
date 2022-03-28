f0ea3af18e6aa14ec56a0abf0f075629d91df2b4
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-03-28 05:08:35 +0000

    add result function to js page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
    userChoice = e.target.id
    userChoiceDisplay.innerHTML = userChoice
    generateComputerChoice()
+   getResult()
 }))
 
 function generateComputerChoice() {
@@ -32,3 +34,57 @@ function generateComputerChoice() {
    computerChoiceDisplay.innerHTML = computerChoice
 }
 
+function getResult() {
+   if(computerChoice === userChoice) {
+      result = 'Tie!'
+   }
+   if(computerChoice === 'rock' && userChoice === 'paper') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'rock' && userChoice === 'scissors') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'rock' && userChoice === 'lizard') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'paper' && userChoice === 'scissors') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'paper' && userChoice === 'spock') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'paper' && userChoice === 'rock') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'scissors' && userChoice === 'rock') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'scissors' && userChoice === 'paper') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'scissors' && userChoice === 'lizard') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'lizard' && userChoice === 'spock') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'lizard' && userChoice === 'paper') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'lizard' && userChoice === 'rock') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'lizard' && userChoice === 'scissors') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'spock' && userChoice === 'rock') {
+      result = 'you lose!'
+   }
+   if(computerChoice === 'spock' && userChoice === 'paper') {
+      result = 'you win!'
+   }
+   if(computerChoice === 'spock' && userChoice === 'scissors') {
+      result = 'you lose!'
+   }
+   resultDisplay.innerHTML = result
+}
