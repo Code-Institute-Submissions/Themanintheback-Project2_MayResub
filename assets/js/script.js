3db66b8b6f8d76273dfb3bdb240542207497e38a
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-03-29 09:29:13 +0000

    Add score incrementing to getRsult function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,20 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+const win = document.querySelector('.win')
+const lose = document.querySelector('.lose')
 let userChoice
 let computerChoice
 let result
+let userscore = 0
+let computerscore = 0
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
    userChoice = e.target.id
    userChoiceDisplay.innerHTML = userChoice
    generateComputerChoice()
    getResult()
+   win()
 }))
 
 function generateComputerChoice() {
@@ -40,51 +45,89 @@ function getResult() {
    }
    if(computerChoice === 'rock' && userChoice === 'paper') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'rock' && userChoice === 'scissors') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'rock' && userChoice === 'lizard') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'paper' && userChoice === 'scissors') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'paper' && userChoice === 'spock') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'paper' && userChoice === 'rock') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'scissors' && userChoice === 'rock') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'scissors' && userChoice === 'paper') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'scissors' && userChoice === 'lizard') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'lizard' && userChoice === 'spock') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'lizard' && userChoice === 'paper') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'lizard' && userChoice === 'rock') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'lizard' && userChoice === 'scissors') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'spock' && userChoice === 'rock') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    if(computerChoice === 'spock' && userChoice === 'paper') {
       result = 'you win!'
+      userscore++
+      win.innerHTML = userscore
    }
    if(computerChoice === 'spock' && userChoice === 'scissors') {
       result = 'you lose!'
+      computerscore++
+      lose.innerHTML = computerscore
    }
    resultDisplay.innerHTML = result
+   
+   // function win() {
+   // userscore++
+   // win.innerHTML = userscore
+   // }
 }
+
