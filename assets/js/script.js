4afd4a7b649dc1c2d44556968de316c71f25ef8d
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-04-01 03:12:04 +0000

    change the structure of the if statments in the getResult function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 }))
 
 function generateComputerChoice() {
-   const randomNumber = Math.floor(Math.random() * 5) + 1
+   const randomNumber = Math.floor(Math.random() * 3) + 1
    
    if (randomNumber === 1) {
       computerChoice = 'rock'
@@ -29,100 +29,70 @@ function generateComputerChoice() {
    if (randomNumber === 3) {
       computerChoice = 'scissors'
    }
-   if (randomNumber === 4) {
-      computerChoice = 'lizard'
-   }
-   if (randomNumber === 5) {
-      computerChoice = 'spock'
-   }
    computerChoiceDisplay.innerHTML = computerChoice
 }
 
 function getResult() {
-   if(computerChoice === userChoice) {
-      result = 'Tie!'
+   
+   if (computerChoice === userChoice) {
+      result = 'Tie'
+   }else if(userChoice === 'rock') {
+      if(computerChoice === 'paper') {
+         result = 'you lose'
+         computerscore++
+         lose.innerHTML = computerscore
+      }else if(computerChoice === 'scissors') {
+         result = 'you win'
+         userscore++
+         win.innerHTML = userscore
+      }else if(computerChoice === 'lizard') {
+         result = 'you win'
+         userscore++
+         win.innerHTML = userscore
+      }else {
+         result = 'you lose'
+         computerscore++
+         lose.innerHTML = computerscore
+      }
    }
-   if(computerChoice === 'rock' && userChoice === 'paper') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
+   else if(userChoice === 'paper') {
+      if(computerChoice === 'rock') {
+         result = 'you win'
+         userscore++
+         win.innerHTML = userscore
+      }else if(computerChoice === 'scissors') {
+         result = 'you lose'
+         computerscore++
+         lose.innerHTML = computerscore
+      }else if(computerChoice === 'lizard') {
+         result = 'you lose'
+         computerscore++
+         lose.innerHTML = computerscore
+      }else{
+         result = 'you win'
+         userscore++
+         win.innerHTML = userscore
+      }
    }
-   if(computerChoice === 'rock' && userChoice === 'scissors') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'rock' && userChoice === 'lizard') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'paper' && userChoice === 'scissors') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
-   }
-   if(computerChoice === 'paper' && userChoice === 'spock') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'paper' && userChoice === 'rock') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
-   }
-   if(computerChoice === 'scissors' && userChoice === 'rock') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
-   }
-   if(computerChoice === 'scissors' && userChoice === 'paper') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'scissors' && userChoice === 'lizard') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'lizard' && userChoice === 'spock') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'lizard' && userChoice === 'paper') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'lizard' && userChoice === 'rock') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
-   }
-   if(computerChoice === 'lizard' && userChoice === 'scissors') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
-   }
-   if(computerChoice === 'spock' && userChoice === 'rock') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
-   }
-   if(computerChoice === 'spock' && userChoice === 'paper') {
-      result = 'you win!'
-      userscore++
-      win.innerHTML = userscore
-   }
-   if(computerChoice === 'spock' && userChoice === 'scissors') {
-      result = 'you lose!'
-      computerscore++
-      lose.innerHTML = computerscore
+   else if(userChoice = 'scissors') {
+      if(computerChoice === 'rock') {
+         result = 'you lose'
+         computerscore++
+         lose.innerHTML = computerscore
+      }else if(computerChoice === 'paper') {
+         result = 'you win'
+         userscore++
+         win.innerHTML = userscore
+      }else if(computerChoice === 'lizard') {
+         result = 'you win'
+         userscore++
+         win.innerHTML = userscore
+      }else{
+         result = 'you lose'
+         computerscore++
+         lose.innerHTML = computerscore
+      }
    }
    resultDisplay.innerHTML = result
-   
 }
 
